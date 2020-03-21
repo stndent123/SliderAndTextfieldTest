e07dd757e340065b1d6e7f6d5f6afb08928a46be
Joseph <stndent123@yahoo.com.tw>
2020-03-21 23:38:57 +0800

    Initial Commit

---
+++ SliderAndTextfieldTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SliderAndTextfieldTest
+//
+//  Created by joseph on 2020/3/21.
+//  Copyright © 2020 joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
