e9d5d7dcf58cca1e040b6d0c9bbee06bc3d573c8
Joseph <stndent123@yahoo.com.tw>
2020-03-22 19:20:04 +0800

    again

--- SliderAndTextfieldTest/ViewController.swift
+++ SliderAndTextfieldTest/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var ShowTextField: UITextField!
+    @IBOutlet weak var WordLengthLabel: UILabel!
+    @IBOutlet weak var FontSizeSlider: UISlider!
+    @IBOutlet weak var WordSizeLabel: UILabel!
+    var fontsize:Float = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func ShowWordSize(_ sender:UITextField) {
+        WordSizeLabel.text = "字體大小" + String(FontSizeSlider.value)
+        WordLengthLabel.text = "字數" + String(ShowTextField.text!.count)
+        
+    }
+    @IBAction func FontSizeSliderChange(_ sender: UISlider) {
+        let font = ShowTextField.font!
+      fontsize = sender.value
+        WordSizeLabel.text = "字體大小" + String(format:"%.2f",fontsize)
+        ShowTextField.font = font.withSize(CGFloat(fontsize))
+    }
+    
 
 }
 
